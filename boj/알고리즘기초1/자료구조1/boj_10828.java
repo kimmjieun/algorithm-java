9d50156cfb05a654eb8d4d227119d195563ee5b9
kimmjieun <tttyyy0101@naver.com>
2021-12-12 19:53:00 +0900

    10828번 스택

---
+++ boj/알고리즘기초1/자료구조1/boj_10828.java
@@ -0,0 +1,56 @@
+package 알고리즘기초1.자료구조1;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class boj_10828 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 스택 사용
+        Stack<Integer> s = new Stack<>();
+
+        int num = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i=0;i<num;i++){
+            String[] input = br.readLine().split(" ");
+
+            switch (input[0]){
+                case "push":
+                    s.push(Integer.parseInt(input[1]));
+                    break;
+                case "pop":
+                    if(s.empty()){
+                        bw.write("-1"+"\n");
+                    }else{
+                        bw.write(s.pop()+"\n");
+                    }
+                    break;
+                case "size":
+                    bw.write(s.size()+"\n");
+                    break;
+                case "empty":
+                    if(s.empty()){
+                        bw.write("1"+"\n");
+                    }else{
+                        bw.write("0"+"\n");
+                    }
+                    break;
+                case "top":
+                    if(s.empty()){
+                        bw.write("-1"+"\n");
+                    }else{
+                        bw.write(s.peek()+"\n");
+                    }
+                    break;
+            }
+
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
