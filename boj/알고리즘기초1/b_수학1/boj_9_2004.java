f99b8afa813f961ff17a5a9a6d3f4bd8a4b1cbe8
kimmjieun <tttyyy0101@naver.com>
2022-01-07 19:10:17 +0900

    BOJ 2004번 조합 0의 개수

---
+++ boj/알고리즘기초1/b_수학1/boj_9_2004.java
@@ -0,0 +1,48 @@
+package 알고리즘기초1.b_수학1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 2004번 조합 0의 개수
+public class boj_9_2004 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        long N = Long.parseLong(st.nextToken());
+        long M = Long.parseLong(st.nextToken());
+
+        // 각각의 승수를 구해준다.
+        long count5 = five_power_n(N) - five_power_n(N - M) - five_power_n(M);
+        long count2 = two_power_n(N) - two_power_n(N - M) - two_power_n(M);
+        System.out.println(Math.min(count5, count2));
+    }
+
+
+    // 5의 승수를 구하는 함수
+    static long five_power_n(long num) {
+        int count = 0;
+
+        while (num >= 5) {
+            count += (num / 5);
+            num /= 5;
+        }
+        return count;
+    }
+
+    // 2의 승수를 구하는 함수
+    static long two_power_n(long num) {
+        int count = 0;
+
+        while (num >= 2) {
+            count += (num / 2);
+            num /= 2;
+        }
+        return count;
+    }
+
+
+}
