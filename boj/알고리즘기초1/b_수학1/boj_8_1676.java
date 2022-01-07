a71b1f378b754110010453332d7ccc3311c52595
kimmjieun <tttyyy0101@naver.com>
2022-01-07 18:34:09 +0900

    BOJ 1676번 팩토리얼 0의 개수

---
+++ boj/알고리즘기초1/b_수학1/boj_8_1676.java
@@ -0,0 +1,22 @@
+package 알고리즘기초1.b_수학1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 1676번 팩토리얼 0의 개수
+public class boj_8_1676 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        int count=0;
+
+        while(num>=5){
+            count += num/5;
+            num/=5;
+        }
+
+        System.out.println(count);
+
+    }
+}
