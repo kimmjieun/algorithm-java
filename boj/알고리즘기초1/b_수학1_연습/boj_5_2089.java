778a89d273d9db2b7f5bb8b40c3425c413e00def
kimmjieun <kimmjieun@gmail.com>
2022-01-10 17:46:01 +0900

    BOJ 2089번 -2진법

---
+++ boj/알고리즘기초1/b_수학1_연습/boj_5_2089.java
@@ -0,0 +1,27 @@
+package 알고리즘기초1.b_수학1_연습;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 2089번 -2진법
+public class boj_5_2089 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int input = Integer.parseInt(br.readLine());
+
+        if (input == 0)
+            System.out.println(0);
+        else {
+            while (input != 1) {
+                sb.append(Math.abs(input % -2));
+                input = (int) Math.ceil((double) input / (-2));
+            }
+            sb.append(input);
+            System.out.println(sb.reverse());
+        }
+
+    }
+}
