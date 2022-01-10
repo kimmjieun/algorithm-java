3ebd2f207da08464b7b317fca3c3c82f6cfe27e6
kimmjieun <kimmjieun@gmail.com>
2022-01-10 15:51:11 +0900

    BOJ 17087번 숨바꼭질6

---
+++ boj/알고리즘기초1/b_수학1_연습/boj_2_17087.java
@@ -0,0 +1,36 @@
+package 알고리즘기초1.b_수학1_연습;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 17087번 숨바꼭질6
+public class boj_2_17087 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int a = Integer.parseInt(st2.nextToken());
+            arr[i] = Math.abs(S - a);
+        }
+
+        int gcd = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            gcd = GCD(gcd, arr[i]);
+        }
+        System.out.println(gcd);
+    }
+
+    public static int GCD(int a, int b) {
+        if (b == 0) return a;
+        else {
+            return GCD(b, a % b);
+        }
+    }
+}
