caefc78db52c236a4ab4c7cf3076c300c8738fc2
kimmjieun <kimmjieun@gmail.com>
2022-01-12 22:53:41 +0900

    BOJ 16194번 카드 구매하기 2

---
+++ boj/알고리즘기초1/c_다이나믹프로그래밍1/boj_6_16194.java
@@ -0,0 +1,31 @@
+package 알고리즘기초1.c_다이나믹프로그래밍1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 16194번 카드 구매하기 2
+public class boj_6_16194 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n + 1];
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = Integer.MAX_VALUE;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.min(dp[i], arr[j] + dp[i - j]);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
