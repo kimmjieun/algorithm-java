5c996c997d16267530cded412c4936863dc891ef
kimmjieun <kimmjieun@gmail.com>
2022-01-12 20:50:23 +0900

    BOJ 9095번 1,2,3 더하기

---
+++ boj/알고리즘기초1/c_다이나믹프로그래밍1/boj_4_9095.java
@@ -0,0 +1,29 @@
+package 알고리즘기초1.c_다이나믹프로그래밍1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 9095번 1,2,3 더하기
+public class boj_4_9095 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[11];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+
+            for (int i = 4; i <= n; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+            }
+
+            System.out.println(dp[n]);
+
+        }
+    }
+}
