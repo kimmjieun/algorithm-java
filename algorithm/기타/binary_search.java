d9fc213fec19ec793d9c02631e3fc48b1e3f4a67
kimmjieun <kimmjieun@gmail.com>
2022-01-16 00:15:07 +0900

    이진 탐색

---
+++ algorithm/기타/binary_search.java
@@ -0,0 +1,26 @@
+package 기타;
+
+public class binary_search {
+    static int[] arr = {1, 3, 5, 7, 8, 10, 20, 35, 99, 100};
+
+    public static void main(String[] args) {
+        System.out.println(binarySearch(5, 0, arr.length - 1));//2
+    }
+
+    static int binarySearch(int key, int low, int high) {
+        int mid;
+
+        if (low <= high) {
+            mid = (low + high) / 2;
+
+            if (key == arr[mid]) {
+                return mid;
+            } else if (key < arr[mid]) {
+                return binarySearch(key, low, mid - 1);
+            } else {
+                return binarySearch(key, mid + 1, high);
+            }
+        }
+        return -1;
+    }
+}
