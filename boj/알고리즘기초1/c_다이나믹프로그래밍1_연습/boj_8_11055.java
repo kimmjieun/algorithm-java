cf4e71ea6a265657bd3c9db05877edf7fd4dfe6e
kimmjieun <kimmjieun@gmail.com>
2022-01-18 15:02:54 +0900

    BOJ 11055번 가장 큰 증가 부분 수열

---
+++ boj/알고리즘기초1/c_다이나믹프로그래밍1_연습/boj_8_11055.java
@@ -0,0 +1,40 @@
+package 알고리즘기초1.c_다이나믹프로그래밍1_연습;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 11055번 가장 큰 증가 부분 수열
+public class boj_8_11055 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        int max = Integer.MIN_VALUE;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0] = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = arr[i];
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i] && dp[i] < dp[j] + arr[i]) {
+                    dp[i] = dp[j] + arr[i];
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+
+    }
+}
