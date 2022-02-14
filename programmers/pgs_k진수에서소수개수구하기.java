967e76faff1907f406c51217f9e03a3dda81e6d4
kimmjieun <kimmjieun@gmail.com>
2022-02-14 21:20:47 +0900

    [PGS] k진수에서 소수 개수 구하기

---
+++ programmers/pgs_k진수에서소수개수구하기.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.sqrt;
+
+public class pgs_k진수에서소수개수구하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        System.out.println(solution(n, k));
+
+
+    }
+
+    public static int solution(int n, int k) {
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            sb.append(n % k);
+            n = n / k;
+        }
+
+        String s = sb.reverse().toString();
+        String[] split = s.split("0");
+
+        for (String x : split) {
+            if (x.equals("")) continue;
+            if (isPrime(Long.parseLong(x))) count++;
+        }
+
+        return count;
+    }
+
+    public static boolean isPrime(long num) {
+        if (num == 1) {
+            return false;
+        }
+        for (int i = 2; i <= sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+
+}
