9bfc6853651be450d093753956f6fca56a20a6c7
kimmjieun <kimmjieun@gmail.com>
2022-03-25 22:15:48 +0900

    [PGS] 실패율

--- programmers/레벨1/실패율.java
+++ programmers/레벨1/실패율.java
@@ -5,31 +5,33 @@ import java.util.*;
 public class 실패율 {
     public int[] solution(int N, int[] stages) {
         int[] answer = new int[N];
-        double users = stages.length;
-        List<double[]> fail_rate = new ArrayList<>();
+        List<double[]> failRate = new ArrayList<>();
 
-        int count = 0;
+        double sum = stages.length;
+
         for (int i = 1; i <= N; i++) {
+            int cnt = 0;
             for (int j = 0; j < stages.length; j++) {
                 if (stages[j] == i) {
-                    count++;
+                    cnt++;
                 }
-
             }
-            if (count == 0) {
-                fail_rate.add(new double[]{i, 0});
-                continue;
-            }
-            fail_rate.add(new double[]{i, count / users});
-            users -= count;
-            count = 0;
-        }
-        fail_rate.sort((a, b) -> Double.compare(b[1], a[1]));
 
-        for (int i = 0; i < fail_rate.size(); i++) {
-            answer[i] = (int) fail_rate.get(i)[0];
+            if (cnt == 0) {
+                failRate.add(new double[]{i, 0});
+            } else {
+                failRate.add(new double[]{i, cnt / sum});
+            }
+            sum -= cnt;
         }
 
+        Collections.sort(failRate, ((o1, o2) -> Double.compare(o2[1], o1[1])));
+
+
+        for (int i = 0; i < failRate.size(); i++) {
+            answer[i] = (int) failRate.get(i)[0];
+        }
         return answer;
     }
+
 }
