eb5a1cf0d8cc3c557372a34d9959ccc569f01ad2
kimmjieun <kimmjieun@gmail.com>
2022-03-28 23:45:55 +0900

    [PGS] 예산

---
+++ programmers/레벨1/예산.java
@@ -0,0 +1,23 @@
+package 레벨1;
+
+import java.util.Arrays;
+
+public class 예산 {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int result = 0;
+        Arrays.sort(d);
+        for (int i = 0; i < d.length; i++) {
+            result += d[i];
+            if (result > budget) {
+                answer = i;
+                break;
+            }
+        }
+
+        if (result <= budget) {
+            answer = d.length;
+        }
+        return answer;
+    }
+}
