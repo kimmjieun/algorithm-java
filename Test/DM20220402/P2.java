f868d798d68b746f50956ba97d44d72a024ede6f
kimmjieun <kimmjieun@gmail.com>
2022-04-06 17:58:53 +0900

    [TEST] DM20220402 P2

--- Test/DM20220402/P2.java
+++ Test/DM20220402/P2.java
@@ -1,105 +1,106 @@
 package DM20220402;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-// 해결안됨
-
 public class P2 {
-    public static List<int[]> list;
-    public static int ans;
-    public static Character[] alpha = {'G', 'R', 'L'};
+    static int n, m;
+    static List<int[]> list;
+    static int ans;
+    static Character[] alpha = {'G', 'R', 'L'};
+    static boolean[][] visit;
     public static int[] dx = {-1, 1, 0, 0};
     public static int[] dy = {0, 0, -1, 1};
-    public static int n, m;
-    public static boolean[][] visit;
+
 
     public static void main(String[] args) {
         Character[][] map = {{'G', '?', 'L', 'L'}, {'R', '?', 'R', '?'}, {'R', 'R', 'R', 'L'}};
         n = 3;
         m = 4;
+        // 5
+
+//        Character[][] map = {{'?','?','G'}, {'R', 'G', 'L'}, {'L', 'L', '?'}};
+//        n = 3;
+//        m = 3;
+//        // 2
+
+//        Character[][] map = {{'G', 'G', '?'}};
+//        n = 1;
+//        m = 3;
+//        // 3
         System.out.println(solution(map));
     }
 
-    public static int solution(Character[][] map) {
+    private static int solution(Character[][] map) {
+        // ?표 개수세기
+        // 백트래킹으로 ?표 채우기
+        // 다 채웠으면 단지번호붙이기 군집 하기
         list = new ArrayList<>();
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (map[i][j] == '?') {
-                    int[] nums = {i, j};
-                    list.add(nums);
+                    int[] arr = {i, j};
+                    list.add(arr);
                 }
             }
         }
 
-//        for(int[] a : list){
-//            System.out.println(a[0]+" ddd");
-//            System.out.println(a[1]+" sss");
-//        }
-
-        ans =0;
+        ans = 0;
         back(0, map);
-
         return ans;
 
     }
 
-    public static void back(int idx, Character[][] map) {
 
-        if (idx == list.size() - 1) {
+    public static void back(int idx, Character[][] map) {
+        if (idx == list.size()) {// ?표 다채웠으면 군집세기
             if (checkGroup(map)) {
-                for (int i = 0; i < n; i++) {
-                    for (int j = 0; j < m; j++) {
-                        System.out.print(map[i][j]+" ");
-                    }
-                    System.out.println();
-                }
-                System.out.println("***");
                 ans++;
             }
             return;
         }
 
-
-        for (Character a : alpha) {
+        for (char a : alpha) {
             int x = list.get(idx)[0];
             int y = list.get(idx)[1];
             map[x][y] = a;
             back(idx + 1, map);
             map[x][y] = '?';
         }
+
     }
 
     public static boolean checkGroup(Character[][] map) {
         visit = new boolean[n][m];
-        for (Character a : alpha) {
-            int cnt = 0;
+        for (char a : alpha) {
+            int count = 0;
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < m; j++) {
                     if (map[i][j] == a && !visit[i][j]) {
-                        cnt++;
                         dfs(i, j, map);
+                        count++;
                     }
                 }
             }
-            if (cnt >= 2) return false;
+            if (count >= 2) return false;
         }
         return true;
     }
 
     public static void dfs(int x, int y, Character[][] map) {
         visit[x][y] = true;
+
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
             if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
             if (visit[nx][ny]) continue;
-            if (map[x][y] != map[nx][ny]) continue;
-            dfs(nx, ny, map);
+            if (map[nx][ny] != map[x][y]) continue;
 
+            dfs(nx, ny, map);
         }
     }
+
+
 }
