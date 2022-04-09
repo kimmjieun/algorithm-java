2d2520cab015f7bf01f773ecbaed3a52d111d3f3
kimmjieun <kimmjieun@gmail.com>
2022-04-09 23:54:01 +0900

    [BOJ] 7576번 토마토

--- boj/그래프탐색/BOJ7576.java
+++ boj/그래프탐색/BOJ7576.java
@@ -8,13 +8,13 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-
 public class BOJ7576 {
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    static int n, m;
+    static int m, n;// 가로칸, 세로칸
     static int[][] map;
     static Queue<int[]> q = new LinkedList<>();
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,8 +25,6 @@ public class BOJ7576 {
         n = Integer.parseInt(st.nextToken());
 
         map = new int[n][m];
-
-
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
@@ -37,50 +35,52 @@ public class BOJ7576 {
             }
         }
 
-        System.out.println(bfs());
+        bfs();
+
+        if (checkZero()) {
+            System.out.println(-1);
+        } else {
+            int max = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    max = Math.max(max, map[i][j]);
+                }
+            }
+            System.out.println(max - 1);
+
+        }
     }
 
-    private static int bfs() {
+    static void bfs() {
         while (!q.isEmpty()) {
             int[] t = q.poll();
             int x = t[0];
             int y = t[1];
+
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
                 if (map[nx][ny] == 0) {
-                    map[nx][ny] = map[x][y] + 1;
                     q.add(new int[]{nx, ny});
+                    map[nx][ny] = map[x][y] + 1;
                 }
             }
         }
-
-        int max = Integer.MIN_VALUE;
-        if (checkZero()) {
-            return -1;
-        } else {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    if (max < map[i][j]) {
-                        max = map[i][j];
-                    }
-                }
-            }
-
-            return max - 1;
-        }
-
 
     }
 
-    private static boolean checkZero() {
+
+    static boolean checkZero() {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (map[i][j] == 0)
+                if (map[i][j] == 0) {
                     return true;
+                }
             }
         }
         return false;
     }
+
+
 }
