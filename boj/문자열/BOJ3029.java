9335c23a410879bd55d6c3d940d6542e8a20794d
kimmjieun <kimmjieun@gmail.com>
2022-04-22 01:05:30 +0900

    [BOJ] 3029. 경고

--- boj/문자열/BOJ3029.java
+++ boj/문자열/BOJ3029.java
@@ -8,32 +8,50 @@ public class BOJ3029 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String input1 = br.readLine();
+        //1. :으로 스플릿
+        String input = br.readLine();
         String input2 = br.readLine();
 
-        String[] s1 = input1.split(":");
+        String[] s = input.split(":");
         String[] s2 = input2.split(":");
 
-        int[] n = new int[3];
+        int[] S = new int[3];
+        int[] S2 = new int[3];
         for (int i = 0; i < 3; i++) {
-            n[i] = Integer.parseInt(s2[i]) - Integer.parseInt(s1[i]);
+            S[i] = Integer.parseInt(s[i]);
+            S2[i] = Integer.parseInt(s2[i]);
         }
-        if (input1.equals(input2)) {
+
+        //2. 시간 계산
+        if (input.equals(input2)) {
             System.out.println("24:00:00");
         } else {
-            if (n[2] < 0) {
-                n[2] += 60;
-                n[1]--;
-            }
-            if (n[1] < 0) {
-                n[1] += 60;
-                n[0]--;
-            }
-            if (n[0] < 0) {
-                n[0] += 24;
+            int re1, re2, re3; // 시 분초
 
+            if (S2[2] < S[2]) {
+                S2[1]--;
+                re3 = S2[2] + 60 - S[2];
+            } else {
+                re3 = S2[2] - S[2];
             }
-            System.out.format("%02d:%02d:%02d", n[0], n[1], n[2]);
+
+            if (S2[1] < S[1]) {
+                S2[0]--;
+                re2 = S2[1] + 60 - S[1];
+            } else {
+                re2 = S2[1] - S[1];
+            }
+
+            if (S2[0] < S[0]) {
+                re1 = S2[0] - S[0] + 24;
+            } else {
+                re1 = S2[0] - S[0];
+            }
+
+            // 3. 출력시 포맷팅
+            System.out.format("%02d:%02d:%02d", re1, re2, re3);
         }
+
     }
+
 }
