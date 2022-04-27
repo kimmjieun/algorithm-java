dae4ebe9a94ac1e2c0425f3b5342dba3f9580cfc
kimmjieun <kimmjieun@gmail.com>
2022-04-27 23:41:48 +0900

    [PGS] 위장

--- programmers/해시/위장.java
+++ programmers/해시/위장.java
@@ -10,11 +10,17 @@ public class 위장 {
             map.put(type, map.getOrDefault(type, 0) + 1);
         }
 
+        // 방법 1) value 목록을 리스트로 만들어 밸류리스트로 사용
         List<Integer> list = new ArrayList<>(map.values());
         int answer = 1;
         for (Integer i : list) {
             answer *= i + 1;
         }
+
+        // 방법 2) keySet으로 value를 가져오는 방법
+//        for(String key :map.keySet()){
+//            answer*=(map.get(key)+1);
+//        }
 
         return answer - 1;
     }
