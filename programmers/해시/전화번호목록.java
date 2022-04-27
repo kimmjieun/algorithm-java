bb4f1528ffeb81fbb652e791259bb82e65c3fa4e
kimmjieun <kimmjieun@gmail.com>
2022-04-27 23:41:58 +0900

    [PGS] 전화번호 목록

--- programmers/해시/전화번호목록.java
+++ programmers/해시/전화번호목록.java
@@ -6,6 +6,11 @@ import java.util.*;
 public class 전화번호목록 {
     public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
+        // 119 11955~ 97674
+        // 123 456 789
+        // 12 123 1235 567 88
+        // 말그대로 문자순 1이 9보다 앞이므로 앞에 오되 문자열의 길이는 상관없다
+
         for (int i = 0; i < phone_book.length - 1; i++) {
             if (phone_book[i + 1].startsWith(phone_book[i])) {
                 return false;
