555c9a209c126b6b7144ba25a3231030c4de0d34
kimmjieun <kimmjieun@gmail.com>
2022-05-06 12:59:15 +0900

    [BOJ] 9012. 괄호

---
+++ boj/기타/BOJ9012.java
@@ -0,0 +1,49 @@
+package 기타;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class BOJ9012 {
+    static Stack<Character> stack;
+    static String s;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+
+        while (n-- > 0) {
+
+            s = br.readLine();
+            stack = new Stack<>();
+            System.out.println(isStack());
+
+        }
+
+    }
+
+    public static String isStack() {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push('(');
+            } else if (s.charAt(i) == ')') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                } else {
+                    return "NO";
+                }
+            }
+        }
+
+        if (stack.isEmpty()) {
+            return "YES";
+        } else {
+            return "NO";
+        }
+
+    }
+}
