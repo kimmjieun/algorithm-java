ef8c2e43b618b4f8912e2aa7cbc00c39aa685bda
kimmjieun <kimmjieun@gmail.com>
2022-05-26 00:21:10 +0900

    [BOJ] 1325. 효율적인 해킹

--- boj/그래프탐색/BOJ1325.java
+++ boj/그래프탐색/BOJ1325.java
@@ -1,22 +1,15 @@
 package 그래프탐색;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class BOJ1325 {
-    static boolean[] visit;
-    static int max = Integer.MIN_VALUE;
-    static int n, m;
-    static int count;
+    static int n,m;
     static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-
-
-    public static void main(String[] args) throws IOException {
+    static boolean[] visit;
+    static int[] result;
+    static int max = Integer.MIN_VALUE;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
@@ -24,48 +17,44 @@ public class BOJ1325 {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        for(int i=0;i<=n;i++){
+        for(int i=0;i<=n;i++) {
             list.add(new ArrayList<>());
         }
-        for (int i = 0; i < m; i++) {
+        for(int i=0;i<m;i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             list.get(b).add(a);
         }
-        int[] result = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            visit = new boolean[n + 1];
-            count = 0;
+        result = new int[n+1];
+        for(int i=1;i<=n;i++) {
+            visit = new boolean[n+1];
             bfs(i);
-            result[i] = count;
-            max = Math.max(count, max);
-
         }
-        for (int i = 1; i <= n; i++) {
-            if (result[i] == max)
-                sb.append(i+" ");
+        for(int i=1;i<=n;i++) {
+            if(result[i]==max) {
+                sb.append(i).append(" ");
+            }
         }
         System.out.println(sb);
-
     }
-
-    public static void bfs(int x) {
+    static void bfs(int x) {
         Queue<Integer> q = new LinkedList<>();
         q.add(x);
-        visit[x] = true;
-        while (!q.isEmpty()) {
-            int v = q.poll();
-            for(int i : list.get(v)){
-                if(!visit[i]){
-                    q.add(i);
-                    visit[i] = true;
+        visit[x]=true;
+        int count=1;
+        while(!q.isEmpty()) {
+            int t = q.poll();
+            for(int v:list.get(t)) {
+                if(!visit[v]) {
+                    visit[v]=true;
+                    q.add(v);
                     count++;
                 }
             }
-
         }
+        result[x]=count;
+        max = Math.max(max, count);
+
     }
-
-
 }
