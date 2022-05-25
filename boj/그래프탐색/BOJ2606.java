0324732f5ddecbdf03d9c65132cafabf8fb9b4c3
kimmjieun <kimmjieun@gmail.com>
2022-05-26 00:21:58 +0900

    [BOJ] 2606. 바이러스

--- boj/그래프탐색/BOJ2606.java
+++ boj/그래프탐색/BOJ2606.java
@@ -1,42 +1,46 @@
 package 그래프탐색;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class BOJ2606 {
+    static int count = 0;
+    static int[][] map;
+    static int n;
+    static boolean[] visit;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-        int[][] map = new int[n + 1][n + 1];
-        boolean[] visit = new boolean[n + 1];
-        for (int i = 0; i < m; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            map[x][y] = map[y][x] = 1;
-        }
+        n = Integer.parseInt(br.readLine());
+        int t = Integer.parseInt(br.readLine());
 
-        int count = 0;
+        map = new int[n + 1][n + 1];
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            map[a][b] = map[b][a] = 1;
+        }
+        visit = new boolean[n + 1];
+        bfs(1);
+        System.out.print(count);
+    }
+
+    // 함수화
+    static void bfs(int x) {
         Queue<Integer> q = new LinkedList<>();
-        q.add(1);
-        visit[1] = true;
+        q.add(x);
+        visit[x] = true;
         while (!q.isEmpty()) {
-            int x = q.poll();
+            int tx = q.poll();
             for (int i = 1; i <= n; i++) {
-                if (map[x][i] == 1 && !visit[i]) {
+                if (!visit[i] && map[tx][i] == 1) {
                     visit[i] = true;
                     q.add(i);
                     count++;
                 }
             }
         }
-
-        System.out.println(count);
     }
 
 }
