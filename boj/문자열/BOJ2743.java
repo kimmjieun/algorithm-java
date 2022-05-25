ad567c1e517e602372a692444cdc2fe3b22605c1
kimmjieun <kimmjieun@gmail.com>
2022-05-26 08:59:04 +0900

    [BOJ] 2643. 단어 길이 재기

---
+++ boj/문자열/BOJ2743.java
@@ -0,0 +1,13 @@
+package 문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(s.length());
+    }
+}
