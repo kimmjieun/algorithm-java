9b01a8de714043d7e5e31c6cad486ae2f10a7f95
kimmjieun <kimmjieun@gmail.com>
2022-05-26 00:22:31 +0900

    [BOJ] 16918. 봄버맨

--- boj/그래프탐색/BOJ16918.java
+++ boj/그래프탐색/BOJ16918.java
@@ -83,3 +83,77 @@ public class BOJ16918 {
     }
 
 }
+
+/*
+위 코드보단 아래 코드가 직관적이다.
+하지만 속도면에선 위 코드가 낫다.
+ */
+
+//public class BOJ16918 {
+//    static int r, c,n;
+//    static Character[][] map;
+//    static int[] dx = {-1,1,0,0};
+//    static int[] dy = {0,0,-1,1};
+//    public static void main(String[] args) throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        StringTokenizer st;
+//        st = new StringTokenizer(br.readLine());
+//
+//        r = Integer.parseInt(st.nextToken());
+//        c = Integer.parseInt(st.nextToken());
+//        n = Integer.parseInt(st.nextToken());
+//
+//        map = new Character[r][c];
+//        for(int i=0;i<r;i++) {
+//            String s= br.readLine();
+//            for(int j=0;j<c;j++) {
+//                map[i][j]=s.charAt(j);
+//
+//            }
+//        }
+//        Queue<int[]> q = new LinkedList<>();
+//
+//        for(int i=2;i<=n;i++) {
+//            if(i%2==0) {
+//                for(int j=0;j<r;j++) {
+//                    for(int k=0;k<c;k++) {
+//                        if(map[j][k]=='O') {
+//                            q.add(new int[] {j,k});
+//                        }
+//                    }
+//                }
+//
+//                for(int k=0;k<r;k++) {
+//                    Arrays.fill(map[k],'O');
+//                }
+//
+//            }else {
+//                while(!q.isEmpty()) {
+//                    int[]t = q.poll();
+//                    int tx = t[0];
+//                    int ty = t[1];
+//                    map[tx][ty]='.';
+//                    for(int k=0;k<4;k++) {
+//                        int nx=dx[k]+tx;
+//                        int ny = dy[k]+ty;
+//                        if(nx>=0&&nx<r&&ny>=0&&ny<c) {
+//                            map[nx][ny]='.';
+//                        }
+//                    }
+//                }
+//            }
+//
+//        }
+//
+//        for(int i=0;i<r;i++) {
+//            for(int j=0;j<c;j++) {
+//                System.out.print(map[i][j]);
+//
+//            }
+//            System.out.println();
+//        }
+//
+//    }
+//}
+//
