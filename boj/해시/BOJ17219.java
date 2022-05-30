7a12aff1ecc6db0f21b03c19c8e836bfa01ccd45
kimmjieun <kimmjieun@gmail.com>
2022-05-30 22:25:11 +0900

    [BOJ] 17219. 비밀번호 찾기

---
+++ boj/해시/BOJ17219.java
@@ -0,0 +1,30 @@
+package 해시;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class BOJ17219 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        HashMap<String, String> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            String a = st.nextToken();
+            String b = st.nextToken();
+            map.put(a, b);
+        }
+        for (int i = 0; i < m; i++) {
+            sb.append(map.get(br.readLine())).append("\n");
+        }
+        System.out.println(sb);
+
+    }
+}
