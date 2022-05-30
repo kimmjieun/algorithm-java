ff6b90272fd09c01f2df4033c88d718aa620cf5e
kimmjieun <kimmjieun@gmail.com>
2022-05-30 22:24:39 +0900

    [BOJ] 14425. 문자열 집합

---
+++ boj/해시/BOJ14425.java
@@ -0,0 +1,26 @@
+package 해시;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ14425 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        HashSet<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+
+        }
+        int count = 0;
+        for (int i = 0; i <m; i++) {
+            if (set.contains(br.readLine())) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
