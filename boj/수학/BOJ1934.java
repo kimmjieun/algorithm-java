1d0a4591de96e46a8a45b2fa0d7b554add98bbdd
kimmjieun <kimmjieun@gmail.com>
2022-06-01 00:54:34 +0900

    [BOJ] 1934. 최소공배수

---
+++ boj/수학/BOJ1934.java
@@ -0,0 +1,30 @@
+package 수학;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ1934 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(lcm(a, b));
+        }
+    }
+
+    public static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
