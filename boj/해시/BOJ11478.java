ea701629ca920993a0c2a965c167c5ad2e711f8c
kimmjieun <kimmjieun@gmail.com>
2022-06-01 22:01:05 +0900

    [BOJ] 11478 서로 다른 부분 문자열의 개수

--- boj/해시/BOJ11478.java
+++ boj/해시/BOJ11478.java
@@ -8,16 +8,25 @@ import java.util.HashSet;
 public class BOJ11478 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
+//        String s = br.readLine();
+//        HashSet<String> set = new HashSet<>();
+//        for (int i = 1; i <= s.length(); i++) {
+//            int j;
+//            for (j = 0; j < s.length() - i; j++) {
+//                set.add(s.substring(j, j + i));
+////                System.out.println(s.substring(j, j + i));
+//            }
+//            set.add(s.substring(j));
+////            System.out.println(s.substring(j));
+//        }
+
+        // 리팩터링
         HashSet<String> set = new HashSet<>();
+        String s = br.readLine();
         for (int i = 1; i <= s.length(); i++) {
-            int j;
-            for (j = 0; j < s.length() - i; j++) {
+            for (int j = 0; j < s.length() - i+1; j++) {
                 set.add(s.substring(j, j + i));
-//                System.out.println(s.substring(j, j + i));
             }
-            set.add(s.substring(j));
-//            System.out.println(s.substring(j));
         }
         System.out.println(set.size());
     }
