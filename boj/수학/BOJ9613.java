7747a9c1060fb4f489ab304f6796c211572544de
kimmjieun <kimmjieun@gmail.com>
2022-06-08 10:53:13 +0900

    [BOJ] 9613 GCD 합

---
+++ boj/수학/BOJ9613.java
@@ -0,0 +1,41 @@
+package 수학;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ9613 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            long sum = 0;
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    sum += gcd(arr[i], arr[j]);
+                }
+            }
+            System.out.println(sum);
+        }
+
+    }
+
+    static int gcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
