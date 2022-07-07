dfa65833631465e35d1180dbbbfbbd23f27b704a
kimmjieun <kimmjieun@gmail.com>
2022-07-07 22:26:19 +0900

    [BOJ] 10546 배부른 마라토너

--- boj/해시/해시셋/BOJ10546.java
+++ boj/해시/해시셋/BOJ10546.java
@@ -8,41 +8,41 @@ import java.util.HashSet;
 import java.util.Iterator;
 
 public class BOJ10546 {
+    //    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int n = Integer.parseInt(br.readLine());
+//        HashMap<String, Integer> map = new HashMap<>();
+//
+//        for (int i = 0; i < n * 2 - 1; i++) {
+//            String name = br.readLine();
+//
+//            if (map.containsKey(name)) {
+//                map.remove(name);
+//            } else {
+//                map.put(name, 1);
+//            }
+//        }
+//        for (String key : map.keySet()) {
+//            System.out.println(key);
+//        }
+//
+//    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        HashMap<String, Integer> map = new HashMap<>();
-
+        HashSet<String> set = new HashSet<>();
         for (int i = 0; i < n * 2 - 1; i++) {
             String name = br.readLine();
-
-            if (map.containsKey(name)) {
-                map.remove(name);
+            if (set.contains(name)) {
+                set.remove(name);
             } else {
-                map.put(name, 1);
+                set.add(name);
             }
         }
-        for (String key : map.keySet()) {
-            System.out.println(key);
-        }
 
+        Iterator iter = set.iterator();
+        System.out.println(iter.next());
     }
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//
-//        int n = Integer.parseInt(br.readLine());
-//        HashSet<String> set  = new HashSet<>();
-//        for(int i=0;i<n*2-1;i++){
-//            String name = br.readLine();
-//            if(set.contains(name)){
-//                set.remove(name);
-//            }else{
-//                set.add(name);
-//            }
-//        }
-//
-//        Iterator iter = set.iterator();
-//        System.out.println(iter.next());
-//    }
 }
