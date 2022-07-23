a8e437748b0fb534470262e242340543073f92fa
kimmjieun <kimmjieun@gmail.com>
2022-07-23 21:53:29 +0900

    [BOJ] 9375 패션왕 신혜빈

--- boj/해시/BOJ9375.java
+++ boj/해시/BOJ9375.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.io.*;
 
 public class BOJ9375 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
@@ -11,24 +12,49 @@ public class BOJ9375 {
         while (t-- > 0) {
             int n = Integer.parseInt(br.readLine());
             HashMap<String, ArrayList<String>> map = new HashMap<>();
-            for (int i = 0; i < n; i++) {
-                String[] s = br.readLine().split(" ");
 
-                if (!map.containsKey(s[1])) {
-                    map.put(s[1], new ArrayList<>());
-                    map.get(s[1]).add(s[0]);
-                } else {
-                    map.get(s[1]).add(s[0]);
+            for (int i = 0; i < n; i++) {
+                String[] str = br.readLine().split(" ");
+
+                if (!map.containsKey(str[1])) {
+                    map.put(str[1], new ArrayList<>());
                 }
+
+                map.get(str[1]).add(str[0]);
             }
+
             int sum = 1;
             for (String key : map.keySet()) {
                 sum *= (map.get(key).size() + 1);
             }
             System.out.println(sum - 1);
-
         }
-
     }
+
+
+        // 쉽게 푸는 법
+//        public static void main(String[] args) throws IOException {
+//            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//            int t = Integer.parseInt(br.readLine());
+//
+//            while (t-- > 0) {
+//                int n = Integer.parseInt(br.readLine());
+//                HashMap<String, Integer> map = new HashMap<>();
+//
+//                for (int i = 0; i < n; i++) {
+//                    String[] str = br.readLine().split(" ");
+//                    map.put(str[1], map.getOrDefault(str[1], 0) + 1);
+//
+//                }
+//
+//                int sum = 1;
+//                for (String key : map.keySet()) {
+//                    sum *= (map.get(key) + 1);
+//                }
+//                System.out.println(sum - 1);
+//            }
+//        }
+
+
 
 }
