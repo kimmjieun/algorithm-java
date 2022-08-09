1c0daa2376f6ded03e71fc6be840680ee1153fd7
kimmjieun <kimmjieun@gmail.com>
2022-08-09 22:35:57 +0900

    [PGS] 2016년

--- programmers/레벨1/P2016년.java
+++ programmers/레벨1/P2016년.java
@@ -2,36 +2,18 @@ package 레벨1;
 
 public class P2016년 {
     public String solution(int a, int b) {
-        int[] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        int days = b;
+
+        int[] days = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] dayOfWeek = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
+
+        int sum = b;
+
         for (int i = 0; i < a - 1; i++) {
-            days += month[i];
-        }
-        String answer = "";
-        switch (days % 7) {
-            case 1:
-                answer = "FRI";
-                break;
-            case 2:
-                answer = "SAT";
-                break;
-            case 3:
-                answer = "SUN";
-                break;
-            case 4:
-                answer = "MON";
-                break;
-            case 5:
-                answer = "TUE";
-                break;
-            case 6:
-                answer = "WED";
-                break;
-            case 0:
-                answer = "THU";
-                break;
+            sum += days[i];
         }
 
+        System.out.println(sum);
+        String answer = dayOfWeek[sum % 7];
         return answer;
     }
 }
