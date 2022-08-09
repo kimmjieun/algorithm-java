e3a59250f5eee6af3f6db325d81a4992df211b26
kimmjieun <kimmjieun@gmail.com>
2022-08-09 22:48:57 +0900

    [PGS] 문자열 다루기 기본

---
+++ programmers/레벨1/문자열다루기기본.java
@@ -0,0 +1,20 @@
+package 레벨1;
+
+public class 문자열다루기기본 {
+    public boolean solution(String s) {
+        boolean answer = true;
+        if (s.length() == 4 || s.length() == 6) {
+            for (int i = 0; i < s.length(); i++) {
+                // System.out.println(s.charAt(i));
+                int n = s.charAt(i) - '0';
+                if (n < 0 || n > 9) {
+                    answer = false;
+                    break;
+                }
+            }
+        } else {
+            answer = false;
+        }
+        return answer;
+    }
+}
