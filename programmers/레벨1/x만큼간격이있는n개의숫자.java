3ae4c57c92201d30f8ab8c81e61f73f096770c74
kimmjieun <kimmjieun@gmail.com>
2022-08-09 23:22:08 +0900

    [PGS] x만큼 간격이 있는 n개의 숫자

---
+++ programmers/레벨1/x만큼간격이있는n개의숫자.java
@@ -0,0 +1,12 @@
+package 레벨1;
+
+public class x만큼간격이있는n개의숫자 {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        answer[0] = x;
+        for (int i = 1; i < n; i++) {
+            answer[i] = answer[i - 1] + x;
+        }
+        return answer;
+    }
+}
