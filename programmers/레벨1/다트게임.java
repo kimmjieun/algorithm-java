d5f1e73088487a0846e05b5505034c4bc3c199ba
kimmjieun <kimmjieun@gmail.com>
2022-08-09 22:40:19 +0900

    [PGS] [1차] 다트게임

--- programmers/레벨1/다트게임.java
+++ programmers/레벨1/다트게임.java
@@ -2,35 +2,44 @@ package 레벨1;
 
 public class 다트게임 {
     public int solution(String dartResult) {
+        int answer = 0;
+        String temp = ""; // 문자열 내 1번의 점수 저장 변수
+        int[] arr = new int[3];// 3번의 점수를 저장할 배열
+        int idx = 0;// 배열 참조위한 Idx
 
-        // 각 라운드에서 획득한 점수
-        int[] round = new int[3];
-        char[] carr = dartResult.toCharArray();
-        // 인덱스 조정을 위해 -1부터 시작 매 반복마다 증가시켜야하는데 첫번째 반복에서는 0이되도록
-        int idx = -1;
-        for (int i = 0; i < carr.length; i++) {
-            // n번재 라운드에 쏜 점수가 0-0인경우
-            if (carr[i] >= '0' && carr[i] <= '9') {
-                idx++;// 이전 라운드에서 현재라운드로 증가된 것 -1 -> 0
-                // 점수 추가
-                round[idx] += Integer.parseInt(String.valueOf(carr[i]));
-                // n번째 라운드에 쏜 점수가 10점인 경우
-                if (i + 1 != carr.length - 1 && carr[i + 1] == '0' && carr[i] == '1') {
-                    round[idx] -= Integer.parseInt(String.valueOf(carr[i]));
-                    round[idx] = 10;
-                    i++;
-                }
-            } else if (carr[i] == 'D') {
-                round[idx] *= round[idx];
-            } else if (carr[i] == 'T') {
-                round[idx] *= round[idx] * round[idx];
-            } else if (carr[i] == '*') {
-                if (idx > 0) round[idx - 1] *= 2;
-                round[idx] *= 2;
-            } else if (carr[i] == '#') {
-                round[idx] *= -1;
+        for (int i = 0; i < dartResult.length(); i++) {
+            switch (dartResult.charAt(i)) {
+                case '*':
+                    arr[idx - 1] *= 2;
+                    if (idx > 1) arr[idx - 2] *= 2;
+                    break;
+                case '#':
+                    arr[idx - 1] *= -1;
+                    break;
+                case 'S':
+                    arr[idx] = (int) Math.pow(Integer.parseInt(temp), 1);
+                    idx++;
+                    temp = "";
+                    break;
+                case 'D':
+                    arr[idx] = (int) Math.pow(Integer.parseInt(temp), 2);
+                    idx++;
+                    temp = "";
+                    break;
+                case 'T':
+                    arr[idx] = (int) Math.pow(Integer.parseInt(temp), 3);
+                    idx++;
+                    temp = "";
+                    break;
+                default:
+                    temp += String.valueOf(dartResult.charAt(i));
+                    break;
             }
         }
-        return round[0] + round[1] + round[2];
+
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+        }
+        return answer;
     }
 }
