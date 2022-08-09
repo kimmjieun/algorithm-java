92ec6458b431bb348cc21ccca6f3f5c5076abe4a
kimmjieun <kimmjieun@gmail.com>
2022-08-09 22:37:23 +0900

    [PGS] 최소직사각형

--- programmers/레벨1/최소직사각형.java
+++ programmers/레벨1/최소직사각형.java
@@ -6,22 +6,20 @@ public class 최소직사각형 {
     public int solution(int[][] sizes) {
         int answer = 0;
 
-
+        int maxX = 0;
+        int maxY = 0;
         for (int i = 0; i < sizes.length; i++) {
-            if (sizes[i][0] > sizes[i][1]) {
-                int tmp = sizes[i][0];
-                sizes[i][0] = sizes[i][1];
-                sizes[i][1] = tmp;
+            // 가로가 작은거 세로가 큰거
+            if (sizes[i][0] >= sizes[i][1]) {
+                maxX = Math.max(maxX, sizes[i][1]);
+                maxY = Math.max(maxY, sizes[i][0]);
+            } else {
+                maxX = Math.max(maxX, sizes[i][0]);
+                maxY = Math.max(maxY, sizes[i][1]);
             }
-        }
-
-        int max0 = 0;
-        int max1 = 0;
-        for (int i = 0; i < sizes.length; i++) {
-            max0 = Math.max(max0, sizes[i][0]);
-            max1 = Math.max(max1, sizes[i][1]);
+            //System.out.println(sizes[i][0]+" "+sizes[i][1]);
 
         }
-        return max0 * max1;
+        return maxX * maxY;
     }
 }
