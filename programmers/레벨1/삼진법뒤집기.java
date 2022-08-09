ad8615377f9eb6af4e3dd8a6af128ba2c6dc01db
kimmjieun <kimmjieun@gmail.com>
2022-08-09 22:31:46 +0900

    [PGS] 3진법 뒤집기

--- programmers/레벨1/삼진법뒤집기.java
+++ programmers/레벨1/삼진법뒤집기.java
@@ -5,18 +5,16 @@ import java.util.*;
 public class 삼진법뒤집기 {
 
     public int solution(int n) {
-        String str = "";
         int answer = 0;
-        while (n != 0) {
-            str += String.valueOf(n % 3);
-            n = n / 3;
-        }
-        for (int i = 0; i < str.length(); i++) {
 
-            answer += (str.charAt(i) - '0') * Math.pow(3, str.length() - i - 1);
-            // System.out.println((str.charAt(i)-'0')+" " +Math.pow(3,str.length()-i-1));
+        String s = Integer.toString(n, 3);
+        StringBuilder sb = new StringBuilder();
+        //   System.out.println(s);
+        String news = sb.append(s).reverse().toString();
+        // System.out.println(news);
+        answer = Integer.parseInt(news, 3);
+        //  System.out.println(answer);
+        return answer;
 
-        }
-        return (int) answer;
     }
 }
