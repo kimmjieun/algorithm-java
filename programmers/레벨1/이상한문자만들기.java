c0b94b0ffcbec3272d494a1d1911544b0f1480ef
kimmjieun <kimmjieun@gmail.com>
2022-08-09 22:56:43 +0900

    [PGS] 이상한 문자 만들기

---
+++ programmers/레벨1/이상한문자만들기.java
@@ -0,0 +1,14 @@
+package 레벨1;
+
+public class 이상한문자만들기 {
+    public String solution(String s) {
+        String answer = "";
+        String[] str = s.split("");
+        int cnt = 0;
+        for (String ss : str) {
+            cnt = ss.contains(" ") ? 0 : cnt + 1;
+            answer += cnt % 2 == 0 ? ss.toLowerCase() : ss.toUpperCase();
+        }
+        return answer;
+    }
+}
