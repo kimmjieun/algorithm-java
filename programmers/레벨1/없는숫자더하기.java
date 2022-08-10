309f8f503d9ef62297cc6b4ae65c94760beeea4b
kimmjieun <kimmjieun@gmail.com>
2022-08-10 16:08:53 +0900

    [PGS] 없는 숫자 더하기

--- programmers/레벨1/없는숫자더하기.java
+++ programmers/레벨1/없는숫자더하기.java
@@ -7,22 +7,21 @@ public class 없는숫자더하기 {
             answer -= n;
         }
 
-        // // 오래걸림
-        // int answer=0;
-        // for(int i=0;i<10;i++){
-        //     if(!contains(numbers,i)){
-        //         answer+=i;
-        //     }
-        // }
-
         return answer;
     }
-    // 다른 방안
-    // public boolean contains(final int[] arr, final int key) {
-    //     return Arrays.stream(arr).anyMatch(i -> i == key);
-    // }
-    // 안됨
-    //   public boolean contains(final int[] arr, final int key) {
-    //     return Arrays.asList(arr).contains(key);
-    // }
+
+    public int solution2(int[] numbers) {
+        int answer = 0;
+        boolean[] num = new boolean[10];
+        for (int i = 0; i < numbers.length; i++) {
+            num[numbers[i]] = true;
+        }
+        for (int i = 0; i < num.length; i++) {
+            if (!num[i]) {
+                answer += i;
+            }
+
+        }
+        return answer;
+    }
 }
