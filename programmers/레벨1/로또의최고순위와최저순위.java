42e35879532af21fb5e1cf164b3bf367bc215033
kimmjieun <kimmjieun@gmail.com>
2022-08-10 15:46:06 +0900

    [PGS] 로또의 최고 순위와 최저 순위

--- programmers/레벨1/로또의최고순위와최저순위.java
+++ programmers/레벨1/로또의최고순위와최저순위.java
@@ -2,35 +2,29 @@ package 레벨1;
 
 public class 로또의최고순위와최저순위 {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = new int[2];
-
+        int count = 0;
         int zero = 0;
-        int equal = 0;
-
         for (int i = 0; i < lottos.length; i++) {
-            if (lottos[i] == 0)
-                zero++;
             for (int j = 0; j < win_nums.length; j++) {
                 if (lottos[i] == win_nums[j]) {
-                    equal++;
+                    count++;
+                    break;
+                }
+                if (lottos[i] == 0) {
+                    zero++;
+                    break;
                 }
             }
         }
-
-        if (equal == 0 && zero == lottos.length) {
-            answer[0] = 1;
-            answer[1] = lottos.length;
-        } else if (equal == lottos.length && zero == 0) {
-            answer[0] = 1;
-            answer[1] = 1;
-        } else if (equal == 0 && zero == 0) {
-            answer[0] = lottos.length;
-            answer[1] = lottos.length;
+        if (zero == 6) {
+            return new int[]{1, 6};
+        } else if (zero == 0 && count == 6) {
+            return new int[]{1, 1};
+        } else if (zero == 0 && count == 0) {
+            return new int[]{6, 6};
         } else {
-            answer[0] = lottos.length - equal - zero + 1;
-            answer[1] = lottos.length - equal + 1;
+            return new int[]{6 - count - zero + 1, 6 - count + 1};
         }
 
-        return answer;
     }
 }
