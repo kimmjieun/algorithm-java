75127d36fb5364614ab4856ae85bdf8e9b383d87
kimmjieun <kimmjieun@gmail.com>
2022-08-18 15:17:52 +0900

    [PGS] 주식 가격

---
+++ programmers/레벨2/기타/주식가격.java
@@ -0,0 +1,17 @@
+package 레벨2.기타;
+
+public class 주식가격 {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                answer[i]++;
+                if (prices[i] > prices[j]) {
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+
+}
