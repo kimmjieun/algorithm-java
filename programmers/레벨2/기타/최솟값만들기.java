8068cf1b51e02098d55186d08acee04bb63293e7
kimmjieun <kimmjieun@gmail.com>
2022-08-23 15:06:09 +0900

    [PGS] 최솟값 만들기

---
+++ programmers/레벨2/기타/최솟값만들기.java
@@ -0,0 +1,19 @@
+package 레벨2.기타;
+
+
+import java.util.*;
+
+class 최솟값만들기 {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for (int i = 0; i < A.length; i++) {
+            answer += A[i] * B[A.length - 1 - i];
+        }
+
+
+        return answer;
+    }
+}
