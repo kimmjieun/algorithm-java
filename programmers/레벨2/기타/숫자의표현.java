c326c2700b682a6c49e6e59a422b2fc55ce4e472
kimmjieun <kimmjieun@gmail.com>
2022-08-24 14:10:49 +0900

    [PGS] 숫자의 표현

---
+++ programmers/레벨2/기타/숫자의표현.java
@@ -0,0 +1,22 @@
+package 레벨2.기타;
+
+
+class 숫자의표현 {
+
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            int sum = 0;
+            for (int j = i; j <= n; j++) {
+                sum += j;
+                if (sum == n) {
+                    answer++;
+                    break;
+                }
+                if (sum > n) break;
+            }
+        }
+        return answer;
+    }
+
+}
