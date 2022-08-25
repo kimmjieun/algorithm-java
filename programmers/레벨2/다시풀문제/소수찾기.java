d1ffaf97ca0a6d0bc01dd54bce1dafb688cbfa5a
kimmjieun <kimmjieun@gmail.com>
2022-08-25 15:23:53 +0900

    [PGS] 소수 찾기

--- programmers/레벨2/다시풀문제/소수찾기.java
+++ programmers/레벨2/다시풀문제/소수찾기.java
@@ -2,26 +2,27 @@ package 레벨2.다시풀문제;
 
 import java.util.*;
 
-public class 소수찾기_dfs {
+public class 소수찾기 {
     int answer = 0;
-    List<Integer> list = new ArrayList<>();
     boolean[] visit;
+    ArrayList<Integer> list = new ArrayList<>();
 
     public int solution(String numbers) {
         for (int i = 1; i <= numbers.length(); i++) {
             visit = new boolean[numbers.length()];
-            back(i, numbers, "");
+            back(i, 0, "", numbers);
         }
         return answer;
     }
 
-    public void back(int target, String numbers, String tmp) {
-
-        if (target == tmp.length()) {
-            int num = Integer.parseInt(tmp);
-            if (isPrime(num) && !list.contains(num)) {
-                list.add(num);
-                answer++;
+    public void back(int target, int depth, String str, String numbers) {
+        if (target == depth) {
+            int n = Integer.parseInt(str);
+            if (!list.contains(n)) {
+                if (isPrime(n)) {
+                    list.add(n);
+                    answer++;
+                }
             }
             return;
         }
@@ -29,21 +30,23 @@ public class 소수찾기_dfs {
         for (int i = 0; i < numbers.length(); i++) {
             if (!visit[i]) {
                 visit[i] = true;
-                tmp += numbers.charAt(i);
-                back(target, numbers, tmp);
-                tmp = tmp.substring(0, tmp.length() - 1);
+                back(target, depth + 1, str + numbers.charAt(i), numbers);
                 visit[i] = false;
             }
         }
     }
+
 
     public boolean isPrime(int n) {
         if (n == 0 || n == 1) {
             return false;
         }
         for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) return false;
+            if (n % i == 0) {
+                return false;
+            }
         }
         return true;
     }
+
 }
