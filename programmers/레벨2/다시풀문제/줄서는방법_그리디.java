405121422886a68254ba4ffd0cf72f7e68d94b67
kimmjieun <kimmjieun@gmail.com>
2022-08-29 21:36:56 +0900

    [PGS] 줄서는 방법

--- programmers/레벨2/다시풀문제/줄서는방법_그리디.java
+++ programmers/레벨2/다시풀문제/줄서는방법_그리디.java
@@ -3,42 +3,31 @@ package 레벨2.다시풀문제;
 
 import java.util.*;
 
-// 효율성 틀림, 다시 풀어봐야함
 class 줄서는방법_그리디 {
-    public int[] answer;
-    public boolean[] visit;
-    public int count = 0;
-    public ArrayList<Integer> list = new ArrayList<>();
 
     public int[] solution(int n, long k) {
-        answer = new int[n];
-        visit = new boolean[n + 1];
-        back(n, k, 0);
-        return answer;
-    }
+        int[] answer = new int[n];
 
-    public void back(int n, long k, int depth) {
-        if (depth == n) {
-            count++;
-            if (count == k) {
-                for (int i = 0; i < n; i++) {
-                    answer[i] = list.get(i);
-                }
-                //System.out.println(list.toString());
-                // list.toArray(new int[n]); // 왜
-            }
-            return;
-        }
+        List<Integer> people = new ArrayList<>();
+        long factorial = 1;
+        int index = 0;
 
         for (int i = 1; i <= n; i++) {
-            if (!visit[i]) {
-                visit[i] = true;
-                list.add(i);
-                back(n, k, depth + 1);
-                list.remove(list.size() - 1);
-                visit[i] = false;
-            }
-
+            factorial *= i;
+            people.add(i);
         }
+
+        k--;
+        while (n > 0) {
+            factorial /= n;
+            int value = (int) (k / factorial);
+            answer[index++] = people.get(value);
+            people.remove(value);
+
+            k %= factorial;
+            n--;
+        }
+
+        return answer;
     }
 }
