10cdd8363873e43843e7bfc346aff88f62f0c721
kimmjieun <kimmjieun@gmail.com>
2022-09-02 19:55:00 +0900

    [PGS] 순위 검색

---
+++ programmers/레벨2/다시풀문제/순위검색_이분탐색.java
@@ -0,0 +1,56 @@
+package 레벨2.다시풀문제;
+
+
+import java.util.*;
+
+class 순위검색_이분탐색 {
+    static HashMap<String, List<Integer>> map;
+
+    public int[] solution(String[] info, String[] query) {
+        int[] answer = new int[query.length];
+        map = new HashMap<String, List<Integer>>();
+
+        for (int i = 0; i < info.length; i++) {
+            String[] p = info[i].split(" ");
+            makeSentence(p, "", 0);
+        }
+
+        for (String key : map.keySet()) {
+            Collections.sort(map.get(key));
+        }
+
+        for (int i = 0; i < query.length; i++) {
+            query[i] = query[i].replaceAll(" and ", "");
+            String[] q = query[i].split(" ");
+            answer[i] = map.containsKey(q[0]) ? binarySearch(q[0], Integer.parseInt(q[1])) : 0;
+        }
+        return answer;
+    }
+
+    static int binarySearch(String key, int score) {
+        List<Integer> list = map.get(key);
+        int start = 0, end = list.size() - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (list.get(mid) < score)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        // key에 해당하는 점수의 총개수 - 점수보다 크거나 같은 처음 index
+        return list.size() - start;
+    }
+
+    public static void makeSentence(String[] p, String str, int cnt) {
+        if (cnt == 4) {
+            if (!map.containsKey(str)) {
+                List<Integer> list = new ArrayList<>();
+                map.put(str, list);
+            }
+            map.get(str).add(Integer.parseInt(p[4]));
+            return;
+        }
+        makeSentence(p, str + "-", cnt + 1);
+        makeSentence(p, str + p[cnt], cnt + 1);
+    }
+}
