7161cd4e810f59cd5e0216229b6a3afe056f8c8c
kimmjieun <kimmjieun@gmail.com>
2022-09-11 17:42:44 +0900

    [PGS] 두개 이하로 다른 비트

---
+++ programmers/레벨2/다시풀문제/두개이하로다른비트_문자열.java
@@ -0,0 +1,32 @@
+package 레벨2.다시풀문제;
+
+
+class 두개이하로다른비트_문자열 {
+    public long[] solution(long[] numbers) {
+        long[] answer = new long[numbers.length];
+        int i = 0;
+        for (long num : numbers) {
+            String bit = Long.toString(num, 2);
+            String result = "";
+            if (num % 2 == 0) {
+                // 짝수인 경우
+                answer[i++] = num + 1;
+            } else {
+                // 홀수인 경우
+                if (bit.contains("0")) {
+                    // 마지막 0 찾기
+                    int idx = bit.lastIndexOf("0");
+                    // 마지막 0을 1로 바꾸고 뒤에 1을 0으로 바꾸기
+
+                    result = bit.substring(0, idx) + "10" + bit.substring(idx + 2);
+                } else {
+                    result = bit.substring(0, 1) + "0" + bit.substring(1);
+                }
+                answer[i++] = Long.parseLong(result, 2);
+            }
+
+        }
+
+        return answer;
+    }
+}
