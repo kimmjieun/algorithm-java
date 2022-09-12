99c9b3630991468a2ab7600f0d932711b3113420
kimmjieun <kimmjieun@gmail.com>
2022-09-12 18:05:47 +0900

    [PGS] 단체 사진 찍기

---
+++ programmers/레벨2/다시풀문제/단체사진찍기_dfs.java
@@ -0,0 +1,47 @@
+package 레벨2.다시풀문제;
+
+
+import java.util.*;
+
+class 단체사진찍기_dfsolution {
+    public int answer = 0;
+    public boolean visit[] = new boolean[8];
+    public String[] arr = {"A", "C", "F", "J", "M", "N", "R", "T"};
+
+    public int solution(int n, String[] data) {
+        back(0, data, "");
+
+        return answer;
+    }
+
+    public void back(int depth, String[] data, String s) {
+        if (depth == 8) {
+            for (String d : data) {
+                char c1 = d.charAt(0);
+                char c2 = d.charAt(2);
+                char c3 = d.charAt(3);
+                int dist = Math.abs(s.indexOf(c1) - s.indexOf(c2)) - 1; // -1안해줘서 헤맴
+                int num = d.charAt(4) - '0';
+                boolean flag = false;
+                if (c3 == '=') {
+                    if (dist == num) flag = true;
+                } else if (c3 == '>') {
+                    if (dist > num) flag = true;
+                } else if (c3 == '<') {
+                    if (dist < num) flag = true;
+                }
+                if (!flag) return;
+            }
+            answer++;
+            return;
+        }
+
+        for (int i = 0; i < 8; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                back(depth + 1, data, s+arr[i]);
+                visit[i] = false;
+            }
+        }
+    }
+}
